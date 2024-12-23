c40d52175bdd1c30ac48b3fa146765d8bacb404f
adi26y <adi26y@gmail.com>
2024-12-23 19:14:01 +0200

    fg

--- Skeleton/src/main/java/bgu/spl/mics/application/objects/LiDarWorkerTracker.java
+++ Skeleton/src/main/java/bgu/spl/mics/application/objects/LiDarWorkerTracker.java
@@ -1,5 +1,5 @@
 package bgu.spl.mics.application.objects;
-
+import java.util.*;
 /**
  * LiDarWorkerTracker is responsible for managing a LiDAR worker.
  * It processes DetectObjectsEvents and generates TrackedObjectsEvents by using data from the LiDarDataBase.
@@ -7,5 +7,40 @@ package bgu.spl.mics.application.objects;
  */
 public class LiDarWorkerTracker {
 
-    // TODO: Define fields and methods.
+    private final int id;
+    private final int frequency;
+    private STATUS status;
+    private List<TrackedObject> lastTrackedObjects;
+
+    public LiDarWorkerTracker(int id, int frequency) {
+        this.id = id;
+        this.frequency = frequency;
+        this.status = STATUS.UP;
+        this.lastTrackedObjects = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public STATUS getStatus() {
+        return status;
+    }
+
+    public void setStatus(STATUS status) {
+        this.status = status;
+    }
+
+    public List<TrackedObject> getLastTrackedObjects() {
+        return Collections.unmodifiableList(lastTrackedObjects);
+    }
+
+    @Override
+    public String toString() {
+        return "LiDARTrackerWorker{id=" + id + ", frequency=" + frequency + ", status=" + status + "}";
+    }
 }
