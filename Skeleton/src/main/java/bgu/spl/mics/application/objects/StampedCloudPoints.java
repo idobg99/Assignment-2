b5610342c8eb530478a10c322ba2d1d6b414e2eb
adi26y <adi26y@gmail.com>
2024-12-25 13:15:04 +0200

    fg

--- Skeleton/src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
+++ Skeleton/src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
@@ -1,9 +1,37 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.*;
+
+
 /**
  * Represents a group of cloud points corresponding to a specific timestamp.
  * Used by the LiDAR system to store and process point cloud data for tracked objects.
  */
 public class StampedCloudPoints {
-    // TODO: Define fields and methods.
+    private final String id;
+    private final int time;
+    private List<List<Double>> cloudPoints;
+
+    public StampedCloudPoints(String id, int time, List<List<Double>> cloudPoints) {
+        this.id = id;
+        this.time = time;
+        this.cloudPoints = new ArrayList<>(cloudPoints);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public List<List<Double>> getCloudPoints() {
+        return Collections.unmodifiableList(cloudPoints);
+    }
+
+    @Override
+    public String toString() {
+        return "StampedCloudPoints{id='" + id + "', time=" + time + ", cloudPoints=" + cloudPoints + "}";
+    }
 }
