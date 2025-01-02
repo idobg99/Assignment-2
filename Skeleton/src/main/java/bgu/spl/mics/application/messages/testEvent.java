57d8d86ceddaa2a755162f833580f429c6c9e1b5
adi26y <adi26y@gmail.com>
2025-01-02 23:12:01 +0200

    fdg

---
+++ Skeleton/src/main/java/bgu/spl/mics/application/messages/testEvent.java
@@ -0,0 +1,26 @@
+package bgu.spl.mics.application.messages;
+
+import bgu.spl.mics.Event;
+
+
+
+public class testEvent implements Event<String> {
+
+    private final String string; // Robot's pose including position, orientation, and time
+
+    /**
+     * Constructs a PoseEvent with a given Pose object.
+     *
+     * @param pose The Pose object representing the robot's position and orientation.
+     */
+    public testEvent() {
+        this.string = "";
+    }
+
+    @Override
+    public String toString() {
+        return "PoseEvent{" +
+                "pose=" + string +
+                '}';
+    }
+}
