53a73224c39c5b644002a154d165db72fab17a7f
adi26y <adi26y@gmail.com>
2025-01-05 00:05:26 +0200

    Create testService.java

---
+++ Skeleton/src/main/java/bgu/spl/mics/application/services/testService.java
@@ -0,0 +1,14 @@
+package bgu.spl.mics.application.services;
+
+import bgu.spl.mics.MicroService;
+
+public class testService extends MicroService {
+
+    public testService(){
+        super("testService");
+    }
+    
+    protected void initialize(){
+
+    }
+}
